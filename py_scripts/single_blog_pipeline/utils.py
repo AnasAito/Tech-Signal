6f6bcabfb988b9da37a69926eb0268be44025a67
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2022-10-23 16:36:53 +0000

    Comment PR1 revisited

--- py_scripts/single_blog_pipeline/utils.py
+++ py_scripts/single_blog_pipeline/utils.py
@@ -4,7 +4,7 @@ from typing import List
 
 from extractnet import Extractor
 from skillNer.skill_extractor_class import SkillExtractor
-from py_scripts.data_structures import BlogMetadata, ParsedSkill
+from py_scripts.data_structures import BlogMetadata, ParsedSkill, Occurrences
 
 
 def parse_content_article(
@@ -25,54 +25,76 @@ def parse_content_article(
         )
 
 
-def parse_skills(
-    content: str,
+def parse_skills_in_paragraph(
+    text: str,
     skill_extractor: SkillExtractor,
-    nb_words_chuncks: int = 40
+    nb_words_chuncks: int = 40,
+    paragraph_id: str = "",
 ) -> List[ParsedSkill]:
     """Parse skills in content.
 
     Parameters
     ----------
-    content : str
-        text to partition.
+    text : str
+        Text from where to parse skills.
 
     skill_extractor : SkillExtractor
         Instance of SkillExtractor used to annotate skills
 
     nb_words_chuncks : int, default 40
-        Number of words to consider when partionning content.
+        Number of words to consider when partionning text.
+
+    paragraph_id : str, default ""
+        id used to identified the paragraph and used to build the ids
+        of skills and occurrences.
 
     Return
     ------
         list_parsed_skills : List[ParsedSkill]
             List of parsed skills.
+
+        list_occurrences : List[Occurrence]
+            List of skill occurrences.
     """
     # averge word size in english is 5
-    list_ptr_chuncks = chunck_content(content, chunck_size=5*nb_words_chuncks)
+    list_ptr_chuncks = chunck_content(text, chunck_size=5*nb_words_chuncks)
     list_parsed_skills: List[ParsedSkill] = []
+    list_occurrences: List[Occurrences] = []
 
     # loop over chunks
     for i in range(len(list_ptr_chuncks)-1):
         # select chunck
-        chunck = content[list_ptr_chuncks[i]:list_ptr_chuncks[i+1]]
+        chunck = text[list_ptr_chuncks[i]:list_ptr_chuncks[i+1]]
 
         # extract skills
         annotations = skill_extractor.annotate(
             text=chunck
         )
 
+        # to avoid creating occurrence with no skills
+        contains_skill = False
         for type_of_match, match_results in annotations["results"].items():
             for skill in match_results:
+                # chunk contains skill
+                contains_skill = True
+
                 parsed_skill: ParsedSkill = {
                     "skill_id": skill["skill_id"],
                     "skill_name": skill["doc_node_value"],
-                    "occurence": chunck,
+                    "occurrence_id": f"{paragraph_id}-{i}",
                 }
 
                 list_parsed_skills.append(parsed_skill)
 
-    return list_parsed_skills
+        # add occurrence if it contains skills
+        if contains_skill:
+            occurrence: Occurrences = {
+                "occurrence_id": f"{paragraph_id}-{i}",
+                "content": chunck
+            }
+            list_occurrences.append(occurrence)
+
+    return list_parsed_skills, list_occurrences
 
 
 def chunck_content(
